e5dda34752084d9d53f26be456cf52eb5373f61b
joao-marco-jf <joaomarcos00512@gmail.com>
2024-09-04 10:36:16 -0300

    Implementa páginas de autenticação

---
+++ src/app/auth/signup/page.tsx
@@ -0,0 +1,12 @@
+import SignForm from "@/components/form";
+
+export default async function Signin() {
+    return (
+        <main className="flex h-screen p-8 px-64">
+            <div className="flex w-full justify-center items-center">
+                <SignForm type="signup" />
+            </div>
+            <div className="w-full h-full bg-gradient-to-br from-zinc-700 to-black rounded-xl"/>
+        </main>     
+    )
+}
